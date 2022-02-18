64820e7de6a4db047820ec2a08a0d3f13cf2b50b
Ben4600 <marques.benjaminb@gmail.com>
2022-02-18 02:34:06 +0100

    First Commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const kBackgoundColor = Color(0xFFF7F7F7);
+const kPrimaryColor = Color(0xFF5CE5C9);
+const kSecondaryColor = Color(0xFF98FAE6);
+const kTextColor = Color(0xFFFFFFFF);
+const font = GoogleFonts.roboto;
+const kTextColor2 = Color(0xFF4E6661);
