5a12745acba90f7293765f16b919237d4352ab0c
Benji22ben <benjamin.marques-balula@etu.univ-smb.fr>
2022-05-21 23:19:07 +0200

    Modif local link

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,5 +8,7 @@ const kGrey = Color(0xFFB5B5B5);
 const kTextColor2 = Color(0xFF4E6661);
 const kShadow = Color(0xA6A6A640);
 const kTextInputBg = Color(0xFFF7F7F7);
-const baseUrl =
-    "http://ec2-15-188-89-241.eu-west-3.compute.amazonaws.com:8000/api/";
+const baseUrl
+// =
+//     "http://ec2-15-188-89-241.eu-west-3.compute.amazonaws.com:8000/api/"
+    = "localhost:8002/api/";
